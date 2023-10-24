b10d0f83415ba95273892e73904c123e956b1ac0
Win-ther <emil.winther@iths.se>
2023-10-24 10:47:35 +0200

    Refactor to switch and methods

--- src/main/java/org/example/tennis/TennisGame1.java
+++ src/main/java/org/example/tennis/TennisGame1.java
@@ -2,8 +2,8 @@ package org.example.tennis;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,48 +13,29 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name))
+            player1Score += 1;
+        else if (playerName.equals(player2Name))
+            player2Score += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1Score == player2Score)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "org.example.tennis.Deuce";
-                    break;
-                
-            }
+            score = equalScore();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player1Score >=4 || player2Score >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = advantageOrWinScore();
         }
         else
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = player1Score;
+                else { score+="-"; tempScore = player2Score;}
                 switch(tempScore)
                 {
                     case 0:
@@ -72,6 +53,26 @@ public class TennisGame1 implements TennisGame {
                 }
             }
         }
+        return score;
+    }
+
+    private String advantageOrWinScore() {
+        return switch (player1Score - player2Score) {
+            case 1 -> "Advantage player1";
+            case -1 -> "Advantage player2";
+            case 2,3,4 -> "Win for player1";
+            default -> "Win for player2";
+        };
+    }
+
+    private String equalScore() {
+        String score;
+        score = switch (player1Score) {
+            case 0 -> "Love-All";
+            case 1 -> "Fifteen-All";
+            case 2 -> "Thirty-All";
+            default -> "org.example.tennis.Deuce";
+        };
         return score;
     }
 }
