bae38f99da4d6a30cf591a85d73e48700413bcad
Win-ther <emil.winther@iths.se>
2023-10-24 11:39:46 +0200

    Extract more methods and rename String

--- src/main/java/org/example/tennis/TennisGame1.java
+++ src/main/java/org/example/tennis/TennisGame1.java
@@ -4,8 +4,8 @@ public class TennisGame1 implements TennisGame {
     
     private int player1Score = 0;
     private int player2Score = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -20,44 +20,27 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (player1Score == player2Score)
-        {
-            score = equalScore();
-        }
-        else if (player1Score >=4 || player2Score >=4)
-        {
-            score = advantageOrWinScore();
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Score;
-                else { score+="-"; tempScore = player2Score;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
+        if (player1Score == player2Score) return equalScore(player1Score);
+        else if (player1Score >=4 || player2Score >=4) return advantageOrWinScore(player1Score - player2Score);
+        else return otherScores(player1Score, player2Score);
     }
 
-    private String advantageOrWinScore() {
-        return switch (player1Score - player2Score) {
+    private String otherScores(int player1Score, int player2Score) {
+        return playerScoreToText(player1Score) + "-" + playerScoreToText(player2Score);
+    }
+
+    private static String playerScoreToText(int playerScore) {
+        return switch (playerScore) {
+            case 0 -> "Love";
+            case 1 -> "Fifteen";
+            case 2 -> "Thirty";
+            case 3 -> "Forty";
+            default -> "";
+        };
+    }
+
+    private String advantageOrWinScore(int scoreDifference) {
+        return switch (scoreDifference) {
             case 1 -> "Advantage player1";
             case -1 -> "Advantage player2";
             case 2,3,4 -> "Win for player1";
@@ -65,14 +48,12 @@ public class TennisGame1 implements TennisGame {
         };
     }
 
-    private String equalScore() {
-        String score;
-        score = switch (player1Score) {
+    private String equalScore(int score) {
+        return switch (score) {
             case 0 -> "Love-All";
             case 1 -> "Fifteen-All";
             case 2 -> "Thirty-All";
-            default -> "org.example.tennis.Deuce";
+            default -> "Deuce";
         };
-        return score;
     }
 }
